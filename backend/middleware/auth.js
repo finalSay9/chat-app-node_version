df18801fab411330574f13b1c79aac3127daea17
finalSay9 <chimwazaevan1@gmail.com>
2026-03-18 13:27:16 +0200

    creating the middlewares

---
+++ backend/middleware/auth.js
@@ -0,0 +1,18 @@
+const jwt  = require('jsonwebtoken');
+module.export = function authMiddleware(req,res,next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({message: 'No token provided'});
+
+    }
+    try {
+        const decode = jwt.verify(token, process.env.JWT_SECRET);
+        request.user = decode;
+        next();
+    } catch (error) {
+        
+    }
+
+}
