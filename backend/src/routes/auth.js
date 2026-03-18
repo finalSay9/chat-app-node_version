c64515d5f3d02f4e70ca09e1e126fb1a92441457
finalSay9 <chimwazaevan1@gmail.com>
2026-03-18 13:41:03 +0200

    creating the endpoints in auth

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -8,3 +8,16 @@ const pool = require('../db/pool');
 const router = express.Router();
 
 //helper function to generate jwt
+
+function generateToken(user) {
+    return jwt.sign({id: user.id,
+        username: user.username,
+        email: user.email
+    },
+    process.env.JWT_SECRET,
+{expiresIn: process.env.JWT_EXPIRES_IN}
+);
+
+}
+
+//ENDPOINTS
