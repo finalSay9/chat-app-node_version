03bbb7dff02b98c8d2c8b809a192d4742c2acc71
finalSay9 <chimwazaevan1@gmail.com>
2026-03-18 20:02:43 +0200

    creating endpoints

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -26,6 +26,43 @@ router.post('/register', async (req, res) => {
     const { username, email, password } = req.body
 
     if( !username || !email || !password ){
-        return res.status(400).json({message: "all filled required")
+        return res.status(400).json(message: "all filled required")
+    }
+
+    if(password.length < 6) {
+        return res.status(400).json({error: 'password must be at least 6 characters'})
+    }
+    try {
+        const passwordHash = await bcrypt.hash(password, 12)
+        const result = await pool.query(
+            `INSERT INTO users (username, email, password_hash)
+             VALUES ($1, $2, $3)
+             RETURNING id, username, email, created_at`,
+             [username, email, passwordHash]
+        );
+
+        const user = result.rows[0]
+        const token = generateToken(user);
+
+        res.status(201).json({user, token})
+
+
+    } catch (err) {
+        if(err.code ==='23505') {
+            return res.status(409).json({error: 'username or email already taken'})
+        }
+        console.error(err);
+        res.status(500).json({ error: 'Server error' });
+        
+    }
+})
+
+//post login
+
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    
+    if(!email || !password) {
+        return res.status(400).json({error: 'email and password are required'})
     }
 })
